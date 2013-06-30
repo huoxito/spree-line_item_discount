2558bccc23269aa0ba8bc2786bf48897611c3bd5
Washington Luiz <huoxito@gmail.com>
2013-06-30 17:50:59 -0300

    Reproduce case where promo doesn't apply
    If promo is not eligible at the time the line item was created and
    becomes eligible for some reason when order is updated later on the line
    item discount is not applied

--- spec/models/spree/order_updater_spec.rb
+++ spec/models/spree/order_updater_spec.rb
@@ -15,11 +15,38 @@ module Spree
       action.save!
     end
 
-    context "populate order with new variant" do
-      before { order.contents.add(variant, 1) }
+    context "eligible promo" do
+      context "populate order with new variant" do
+        before { order.contents.add(variant, 1) }
 
-      it "applies existing line item discounts" do
-        expect(order.total.to_f).to_not eql variant.price.to_f
+        it "applies existing line item discounts" do
+          expect(order.total.to_f).to_not eql variant.price.to_f
+        end
+      end
+    end
+
+    context "promo not eligible when item is first created" do
+      # Order total needs to be greater than 30
+      let(:rule) { Promotion::Rules::ItemTotal.create(preferred_amount: 30, preferred_operator: "gt") }
+
+      before do
+        promotion.rules << rule
+        expect(variant.price).to be < rule.preferred_amount
+        order.contents.add(variant, 1)
+      end
+
+      it "doesn't apply existing discount to order total" do
+        expect(order.total.to_f).to eql variant.price.to_f
+      end
+
+      context "promo becomes eligible" do
+        before do
+          order.contents.add(variant, 8)
+        end
+
+        it "applies existing discount to order total" do
+          expect(order.total.to_f).to_not eql (variant.price * 9).to_f
+        end
       end
     end
   end
